c77238de5aa341ed74ebf26381fb2af632d6d494
Kyle Anderson <kyleanderson@faceplacephoto.com>
2018-09-28 15:07:59 -0400

    yarn yelling at trailing comma

--- src/main/index.js
+++ src/main/index.js
@@ -27,7 +27,7 @@ const electronConfig = {
   URL_LAUNCHER_HEIGHT: parseInt(process.env.URL_LAUNCHER_HEIGHT || 1080, 10),
   URL_LAUNCHER_TITLE: process.env.URL_LAUNCHER_TITLE || 'RESIN.IO',
   URL_LAUNCHER_URL: process.env.URL_LAUNCHER_URL || winURL,
-  URL_LAUNCHER_ZOOM: parseFloat(process.env.URL_LAUNCHER_ZOOM || 1.0),
+  URL_LAUNCHER_ZOOM: parseFloat(process.env.URL_LAUNCHER_ZOOM || 1.0)
 }
 
 // enable touch events if your device supports them
